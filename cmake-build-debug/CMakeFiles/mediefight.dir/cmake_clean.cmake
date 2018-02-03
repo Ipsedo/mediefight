a44d71d247888504ad18ac42bd41124b212f1d9b
Ipsedo <7268@samu>
2018-02-03 04:53:37 +0100

    enlever bons bails .idea

---
+++ cmake-build-debug/CMakeFiles/mediefight.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mediefight.dir/src/drawable/modelvbo.cpp.o"
+  "CMakeFiles/mediefight.dir/src/drawable/objmtlvbo.cpp.o"
+  "CMakeFiles/mediefight.dir/src/main.cpp.o"
+  "CMakeFiles/mediefight.dir/src/utils/res.cpp.o"
+  "CMakeFiles/mediefight.dir/src/utils/shader.cpp.o"
+  "mediefight.pdb"
+  "mediefight"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mediefight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
