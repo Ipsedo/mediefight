9a2ca984304fdec3b6eb99a57eb9a39c344e994a
Ipsedo <7268@samu>
2018-10-20 16:47:57 +0200

    add parse fct

---
+++ src/utils/parsing/parse.cpp
@@ -0,0 +1,14 @@
+//
+// Created by samuel on 17/02/18.
+//
+
+#include <cstring>
+#include "parse.h"
+
+float parseFloat(ifstream *file) {
+    char tmp[4];
+    (*file).read(tmp, 4);
+    float res;
+    memcpy(&res, tmp, sizeof(char) * 4);
+    return res;
+}
---
+++ src/utils/parsing/parse.h
@@ -0,0 +1,15 @@
+//
+// Created by samuel on 17/02/18.
+//
+
+#ifndef MEDIEFIGHT_PARSE_H
+#define MEDIEFIGHT_PARSE_H
+
+
+#include <fstream>
+
+using namespace std;
+
+float parseFloat(ifstream *file);
+
+#endif //MEDIEFIGHT_PARSE_H
