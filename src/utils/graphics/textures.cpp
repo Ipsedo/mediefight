4ed3574b050712b5ea9de746f04f2e8f6d84464b
Ipsedo <7268@samu>
2018-10-22 16:52:08 +0200

    set RGB

--- src/utils/graphics/textures.cpp
+++ src/utils/graphics/textures.cpp
@@ -6,6 +6,6 @@
 
 imgRGB loadImage(string imageFile) {
 	int width, height;
-	unsigned char * img = SOIL_load_image(imageFile.c_str(), &width, &height, 0, SOIL_LOAD_AUTO);
+	unsigned char * img = SOIL_load_image(imageFile.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
 	return {width, height, img};
 }
