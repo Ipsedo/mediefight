13e52ca09ebf9fff7881354bb7c24910c8628461
samuel <berriens@gmail.com>
2018-10-22 11:02:18 +0200

    avancé shader -> faire tengeant et bi-tengeante

--- res/shaders/normal_map_fs.glsl
+++ res/shaders/normal_map_fs.glsl
@@ -10,15 +10,16 @@ uniform float u_light_coef;
 varying vec3 v_Position;
 varying vec2 v_TexCoord;
 
-vec4 toNormal(vec4 angles) {
-    return vec4(2.0 * vec3(angles) - 1.0, 0.0);
+vec3 toNormal(vec3 angles) {
+    vec3 res = normalize(2.0 * angles - 1.0);
+    return res;
 }
 
 void main() {
-    vec4 normalColor = texture2D(u_normalMap, v_TexCoord);
-    vec4 colors = texture2D(u_tex, v_TexCoord);
+    vec3 normalColor = texture2D(u_normalMap, v_TexCoord).rgb;
+    vec3 colors = texture2D(u_tex, v_TexCoord).rgb;
 
-    vec3 normal = vec3(u_MVMatrix * toNormal(normalColor));
+    vec3 normal = toNormal(normalColor);
 
     float distance = length(u_LightPos - v_Position);
 
@@ -27,5 +28,5 @@ void main() {
     float diffuse = max(dot(normal, lightVector), 0.1) * u_light_coef;
 
     diffuse = diffuse * (1.0 / (1.0 + (u_distance_coef * distance * distance)));
-    gl_FragColor = colors * diffuse;
+    gl_FragColor = vec4(colors, 1.0) * diffuse;
 }
