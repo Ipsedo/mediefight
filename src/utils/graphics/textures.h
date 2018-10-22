2758d22cf3187ae2b3ea79ba59c1743503d411eb
samuel <berriens@gmail.com>
2018-10-22 11:02:35 +0200

    use soil

--- src/utils/graphics/textures.h
+++ src/utils/graphics/textures.h
@@ -5,30 +5,19 @@
 #ifndef MEDIEFIGHT_TEXTURES_H
 #define MEDIEFIGHT_TEXTURES_H
 
-#include <png.h>
 #include <glm/glm.hpp>
 #include <vector>
 #include <string>
+#include <SOIL/SOIL.h>
 
 using namespace std;
 
 struct imgRGB {
 	int width;
 	int height;
-	float *colors;
+	unsigned char *colors;
 };
 
-struct libpng_image {
-	png_uint_32 width;
-	png_uint_32 height;
-	png_uint_32 bitdepth;
-	png_uint_32 channels;
-	char *data;
-	png_bytep *rowPtrs;
-};
-
-libpng_image readPNG(std::string pngName);
-
-imgRGB toColoredImage(libpng_image image);
+imgRGB loadImage(string imageFile);
 
 #endif //MEDIEFIGHT_TEXTURES_H
