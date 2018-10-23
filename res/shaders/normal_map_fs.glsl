56bd8f7e39b15a2c8d70dcf0d74093137e5da6e0
samuel <berriens@gmail.com>
2018-10-23 17:57:56 +0200

    shaders ok

--- res/shaders/normal_map_fs.glsl
+++ res/shaders/normal_map_fs.glsl
@@ -12,17 +12,17 @@ varying mat3 v_TBN;
 
 void main() {
     vec3 normalColor = texture2D(u_normalMap, v_TexCoord).rgb;
-    vec3 color = texture2D(u_tex, v_TexCoord).rgb;
+    vec3 texColor = texture2D(u_tex, v_TexCoord).rgb;
 
     vec3 normal = normalize(v_TBN * normalize(2.0 * normalColor - 1.0));
 
     float distance = length(u_LightPos - v_Position);
 
-    vec3 lightVector = v_TBN * normalize(u_LightPos - v_Position);
+    vec3 lightVector = normalize(v_TBN * normalize(u_LightPos - v_Position));
 
     float diffuse = max(dot(normal, lightVector), 0.1) * u_light_coef;
 
     diffuse = diffuse * (1.0 / (1.0 + (u_distance_coef * distance * distance)));
 
-    gl_FragColor = vec4(color, 1.0) * diffuse;
+    gl_FragColor = vec4(texColor, 1.0) * diffuse;
 }
