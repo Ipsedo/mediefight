9d7bbfb2de921085c2bbb15ffd619d0a6cf4627c
samuel <berriens@gmail.com>
2018-10-23 22:49:05 +0200

    tuto fini

--- res/shaders/normal_map_fs.glsl
+++ res/shaders/normal_map_fs.glsl
@@ -1,28 +1,69 @@
+#version 150
 precision mediump float;
 uniform sampler2D u_tex;
 uniform sampler2D u_normalMap;
 
 uniform vec3 u_LightPos;
+uniform vec3 u_cam_pos;
 uniform float u_distance_coef;
 uniform float u_light_coef;
 
 varying vec3 v_Position;
 varying vec2 v_TexCoord;
-varying mat3 v_TBN;
+varying vec3 v_Normal;
+
+// https://geeks3d.developpez.com/normal-mapping-glsl/
+mat3 cotangent_frame(vec3 N, vec3 p, vec2 uv)
+{
+    // récupère les vecteurs du triangle composant le pixel
+    vec3 dp1 = dFdx( p );
+    vec3 dp2 = dFdy( p );
+    vec2 duv1 = dFdx( uv );
+    vec2 duv2 = dFdy( uv );
+
+    // résout le système linéaire
+    vec3 dp2perp = cross( dp2, N );
+    vec3 dp1perp = cross( N, dp1 );
+    vec3 T = dp2perp * duv1.x + dp1perp * duv2.x;
+    vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
+
+    // construit une trame invariante à l'échelle
+    float invmax = inversesqrt( max( dot(T,T), dot(B,B) ) );
+    return mat3( T * invmax, B * invmax, N );
+}
+
+vec3 get_normal( vec3 N, vec3 V, vec2 texcoord )
+{
+    // N, la normale interpolée et
+    // V, le vecteur vue (vertex dirigé vers l'œil)
+    vec3 map = texture2D(u_normalMap, texcoord ).rgb;
+    mat3 TBN = cotangent_frame(N, -V, texcoord);
+    return normalize(TBN * map);
+}
 
 void main() {
     vec3 normalColor = texture2D(u_normalMap, v_TexCoord).rgb;
     vec3 texColor = texture2D(u_tex, v_TexCoord).rgb;
 
-    vec3 normal = normalize(v_TBN * normalize(2.0 * normalColor - 1.0));
+    vec3 lightVector = u_LightPos - v_Position;
+    vec3 eyeVec = u_cam_pos - v_Position;
 
-    float distance = length(u_LightPos - v_Position);
+    vec2 uv = v_TexCoord;
 
-    vec3 lightVector = normalize(v_TBN * normalize(u_LightPos - v_Position));
+    vec3 N = normalize(v_Normal);
+    vec3 L = normalize(lightVector);
+    vec3 V = normalize(eyeVec);
+    vec3 PN = get_normal(N, V, uv);
 
-    float diffuse = max(dot(normal, lightVector), 0.1) * u_light_coef;
+    vec3 final_color = vec3(0.0);
+    float lambertTerm = dot(PN, L);
+    if (lambertTerm > 0.0) {
+        final_color = lambertTerm * texColor;
 
-    diffuse = diffuse * (1.0 / (1.0 + (u_distance_coef * distance * distance)));
-
-    gl_FragColor = vec4(texColor, 1.0) * diffuse;
+        /*vec3 E = normalize(Vertex_EyeVec.xyz);
+        vec3 R = reflect(-L, PN);
+        float specular = pow( max(dot(R, E), 0.0), material_shininess);
+        final_color += light_specular * material_specular * specular;*/
+    }
+    gl_FragColor = vec4(final_color, 1.0);
 }
--- res/shaders/normal_map_vs.glsl
+++ res/shaders/normal_map_vs.glsl
@@ -4,23 +4,18 @@ uniform mat4 u_MVMatrix;
 
 attribute vec3 a_Position;
 attribute vec3 a_Normal;
-attribute vec3 a_Tangent;
-attribute vec3 a_BiTangent;
+//attribute vec3 a_Tangent;
+//attribute vec3 a_BiTangent;
 attribute vec2 a_TexCoord;
 
 varying vec3 v_Position;
 varying vec2 v_TexCoord;
-varying mat3 v_TBN;
+varying vec3 v_Normal;
 
 void main() {
     v_Position = vec3(u_MVMatrix * vec4(a_Position, 1.0));
-
     v_TexCoord = a_TexCoord;
-
-    vec3 T = normalize(vec3(u_MVMatrix * vec4(a_Tangent,   0.0)));
-    vec3 B = normalize(vec3(u_MVMatrix * vec4(a_BiTangent, 0.0)));
-    vec3 N = normalize(vec3(u_MVMatrix * vec4(a_Normal,    0.0)));
-    v_TBN = transpose(mat3(T, B, N));
+    v_Normal = normalize(vec3(u_MVMatrix * vec4(a_Normal, 0.0)));
 
     gl_Position = u_MVPMatrix * vec4(a_Position, 1.0);
 }
