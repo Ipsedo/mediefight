ae83280eb608e10241fb0cf8ba09914a76cfa4c6
samuel <berriens@gmail.com>
2018-10-23 17:58:35 +0200

    test normal  map

--- src/main.cpp
+++ src/main.cpp
@@ -48,12 +48,12 @@ int main(int argc, char** argv) {
                         getResFolder() + "/models/snow_baleine_mtl.mtl",
                         true);
 	NormalMapModel squareMap(getResFolder() + "/models/cube.obj",
-			getResFolder() + "/textures/151.png",
-							 getResFolder() + "/textures/Q5660.png");
+			getResFolder() + "/textures/white.png",
+							 getResFolder() + "/textures/Q566o.png");
 
     glm::mat4 projectionMatrix = glm::frustum(-1.f , 1.f , -768.f / 1024.f , 768.f / 1024.f , 1.0f, 50.0f);
     glm::mat4 viewMatrix = glm::lookAt(
-            glm::vec3(1., 0., -1.),
+            glm::vec3(0., 0., -1.),
             glm::vec3(0., 0., 5.),
             glm::vec3(0,1,0)
     );
@@ -69,16 +69,17 @@ int main(int argc, char** argv) {
 	float angleCam = 0.f;
     float angle = 0.0f;
     while (!glfwWindowShouldClose (window)) {
-        glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle+=1e-2, glm::vec3(0.f, 1.f, 0.f));
+        glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle/*+=1e-2*/, glm::vec3(0.5f, 0.5f, 0.f));
         glm::mat4 translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 2.f));
         glm::vec3 cameraPosition = glm::vec3(0., 0., -1.);
         glm::mat4 modelMatrix = translate * rotation;
         glm::mat4 mvMatrix =  viewMatrix * modelMatrix;
         glm::mat4 mvpMatrix = projectionMatrix * mvMatrix;
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        //objVBO.draw(mvpMatrix, mvMatrix, glm::vec3(0.0, 0.0, 0.0));
+		angleCam += 1e-2f;
+        //objVBO.draw(mvpMatrix, mvMatrix, glm::vec3(50.f*cos(angleCam),0.f,50.f*sin(angleCam)));
         //objMtlVBO.draw(mvpMatrix, mvMatrix, glm::vec3(0.f), cameraPosition);
-		squareMap.draw(mvpMatrix, mvMatrix, modelMatrix, glm::vec3(0.f,0.f,-5.f));
+		squareMap.draw(mvpMatrix, mvMatrix, modelMatrix, glm::vec3(5.f*cos(angleCam),0.f,5.f*sin(angleCam)));
         glfwSwapBuffers (window);
         glfwPollEvents();
         this_thread::sleep_for(chrono::milliseconds(1000 / 60));
