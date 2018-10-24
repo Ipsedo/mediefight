ccbc9df64518d2d3f6281f740e20e176f0744e37
samuel <berriens@gmail.com>
2018-10-24 13:57:54 +0200

    test sponge

--- src/main.cpp
+++ src/main.cpp
@@ -47,13 +47,13 @@ int main(int argc, char** argv) {
     ObjMtlVBO objMtlVBO(getResFolder() + "/models/snow_baleine_obj.obj",
                         getResFolder() + "/models/snow_baleine_mtl.mtl",
                         true);
-	NormalMapModel squareMap(getResFolder() + "/models/cube.obj",
+	NormalMapModel squareMap(getResFolder() + "/models/Sponge.obj",
 			getResFolder() + "/textures/white.png",
-							 getResFolder() + "/textures/Q566o.png");
+							 getResFolder() + "/textures/Map__5_Normal_Bump.png");
 
     glm::mat4 projectionMatrix = glm::frustum(-1.f , 1.f , -768.f / 1024.f , 768.f / 1024.f , 1.0f, 50.0f);
     glm::mat4 viewMatrix = glm::lookAt(
-            glm::vec3(0., 0., -4.),
+            glm::vec3(0., 0., -2.),
             glm::vec3(0., 0., 1.),
             glm::vec3(0,1,0)
     );
@@ -66,21 +66,19 @@ int main(int argc, char** argv) {
     glDepthFunc(GL_LEQUAL);
     glDepthMask(GL_TRUE);
 
-	float angleLight = 0.f, rangeLight = 20.f;
     float angle = 0.0f;
     while (!glfwWindowShouldClose (window)) {
-        glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle+=1e-3, glm::vec3(0.0f, 1.f, 0.f));
+        glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle+=1e-3, glm::vec3(0.5f, 0.5f, 0.f));
         glm::mat4 translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 2.f));
-		glm::mat4 scale = glm::scale(glm::mat4(1.f), glm::vec3(5.f));
+		glm::mat4 scale = glm::scale(glm::mat4(1.f), glm::vec3(1e-1f));
         glm::vec3 cameraPosition = glm::vec3(0., 0., -1.);
         glm::mat4 modelMatrix = scale * translate * rotation;
         glm::mat4 mvMatrix =  viewMatrix * modelMatrix;
         glm::mat4 mvpMatrix = projectionMatrix * mvMatrix;
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//angleCam += 1e-2f;
         //objVBO.draw(mvpMatrix, mvMatrix, glm::vec3(50.f*cos(angleCam),0.f,50.f*sin(angleCam)));
         //objMtlVBO.draw(mvpMatrix, mvMatrix, glm::vec3(0.f), cameraPosition);
-		squareMap.draw(mvpMatrix, mvMatrix, glm::vec3(rangeLight*cos(angleLight),0.f,rangeLight*sin(angleLight)), cameraPosition);
+		squareMap.draw(mvpMatrix, mvMatrix, glm::vec3(0.f), cameraPosition);
         glfwSwapBuffers (window);
         glfwPollEvents();
         this_thread::sleep_for(chrono::milliseconds(1000 / 60));
